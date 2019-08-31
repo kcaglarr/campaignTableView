460c60be41f63c71213dc6e64a678dd31a0da74f
Koray Yıldız <koray.yildizz@gmail.com>
2019-08-30 23:12:07 -0700

    Initial Commit

---
+++ MVCExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCExample
+//
+//  Created by Kerim CAGLAR on 30.08.2019.
+//  Copyright © 2019 Kerim CAGLAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
